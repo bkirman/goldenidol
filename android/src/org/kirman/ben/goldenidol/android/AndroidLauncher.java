7959cfd819ac5a380ef0c63339cb8bdbfd09a93f
Ben Kirman <ben@kirman.org>
2014-10-29 21:26:58 +0000

    initial import

---
+++ android/src/org/kirman/ben/goldenidol/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.kirman.ben.goldenidol.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.kirman.ben.goldenidol.GoldenIdol;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GoldenIdol(), config);
+	}
+}
